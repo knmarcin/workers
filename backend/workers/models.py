3e723b60698cc1d2f190f92c69d4dbf381150010
knmarcin <33230423+knmarcin@users.noreply.github.com>
2022-02-10 11:58:19 +0100

    Update models.py

--- backend/workers/models.py
+++ backend/workers/models.py
@@ -5,7 +5,7 @@ class Worker(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     profession = models.CharField(max_length=50)
-    age = models.DateField()
+    age = models.DateField(label='Birthdate YYYY-MM-DD')
     picture = models.ImageField(upload_to='avatars/', blank=True)
 
     def __str__(self):
